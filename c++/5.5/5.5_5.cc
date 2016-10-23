c79ad6c91c325ac1a7a91ffb8924edeb43ffe86b
AJ <ajohnl@protonmail.com>
2016-10-23 22:50:37 +0000

    added moar

---
+++ c++/5.5/5.5_5.cc
@@ -0,0 +1,11 @@
+/*5. Desarrolle un programa que lea de la entrada estandar un vector de enteros y deter- ´
+ * mine el mayor elemento del vector.*/
+
+#include <stdio.h>
+#include <iostream>
+
+int main{using std::cout;using std::cin;
+	int t=0,size;
+	cout << "Introduce numero de elemenots";
+	cin size;
+	int
